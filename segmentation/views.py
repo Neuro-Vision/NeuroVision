c7015b0384c92182300e5f146ec483113cc30278
mihir-parmar <mihir.parmar@universalcoe.edu.in>
2021-10-06 18:26:07 +0530

    Integration step 1

--- segmentation/views.py
+++ segmentation/views.py
@@ -86,7 +86,7 @@ def predict(request):
         
         og = nib.load('segmentation/static/upload/flair.nii')
         nft_img = nib.Nifti1Image(prediction, og.affine)
-        nib.save(nft_img, 'segmentation/static/upload/predicted'  + '.nii')
+        nib.save(nft_img, 'predicted'  + '.nii')
 
 
         # return render(request, 'segmentation/slicedrop/index.html', {'data': dummy[0]})
