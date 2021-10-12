6e4434bb0c061fed6f85f3ba02b24414231d8042
Harsh Patel <harshpatel151218@gmail.com>
2021-10-12 18:04:27 +0530

    Update redirects

--- segmentation/views.py
+++ segmentation/views.py
@@ -93,7 +93,7 @@ def predict(request):
 
         # return render(request, "segmentation/plot3D.html", context={'fig': fig.to_html()})
 
-        return HttpResponseRedirect(reverse('option'))
+        return redirect('/option')
 
         # return render(request, 'segmentation/slicedrop/index.html', {'data': dummy[0]})
         # return render(request, 'segmentation/index.html')
